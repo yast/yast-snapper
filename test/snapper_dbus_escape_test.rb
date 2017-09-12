d86b6c62208672ead6a820f3d5dba6bfbb59e05b
Imobach González Sosa <igonzalezsosa@suse.com>
2017-09-12 09:18:52 +0100

    Add a test_helper for tests

--- test/snapper_dbus_escape_test.rb
+++ test/snapper_dbus_escape_test.rb
@@ -1,42 +1,26 @@
 #!/usr/bin/env rspec
-# coding: utf-8
 
-ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
-
-require "yast"
+require_relative "test_helper"
 
 Yast.import "Snapper"
 
-
 describe "SnapperDbus#escape" do
-
-
   it "call with nothing special" do
-
     expect(Yast::SnapperDbus.send(:escape, "hello")).to eq("hello")
-
   end
 
 
   it "call with UTF-8" do
-
     expect(Yast::SnapperDbus.send(:escape, "schön")).to eq("sch\\xc3\\xb6n".force_encoding(Encoding::ASCII_8BIT))
-
   end
 
 
   it "call with slash" do
-
     expect(Yast::SnapperDbus.send(:escape, "\\")).to eq("\\\\")
-
   end
 
 
   it "call with a hash" do
-
     expect(Yast::SnapperDbus.send(:escape, { "schön" => "hier" })).to eq({ "sch\\xc3\\xb6n".force_encoding(Encoding::ASCII_8BIT) => "hier" })
-
   end
-
-
 end
--- test/snapper_userdata_to_string_test.rb
+++ test/snapper_userdata_to_string_test.rb
@@ -1,34 +1,19 @@
 #!/usr/bin/env rspec
 
-ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
-
-require "yast"
+require_relative "test_helper"
 
 Yast.import "Snapper"
 
-
 describe "Snapper#userdata_to_string" do
-
-
   it "call with empty userdata" do
-
     expect(Yast::Snapper.userdata_to_string({ })).to eq("")
-
   end
-
 
   it "call with simple userdata" do
-
     expect(Yast::Snapper.userdata_to_string({ "hello" => "world" })).to eq("hello=world")
-
   end
-
 
   it "call with complex userdata" do
-
     expect(Yast::Snapper.userdata_to_string({ "a" => "1", "b" => "2" })).to eq("a=1, b=2")
-
   end
-
-
 end
