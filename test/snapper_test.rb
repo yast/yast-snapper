c650d571cdf644bd00d421786bcb48edba68b872
David Díaz González <dgonzalez@suse.de>
2022-01-24 09:17:11 +0000

    Stop using last argument as keyword parameters
    To avoid crashing with an "no implicit conversion of Hash into String
    (TypeError)" in Ruby >= 3, already warned in previous versions.
    
    https://bugzilla.suse.com/show_bug.cgi?id=1195021

--- test/snapper_test.rb
+++ test/snapper_test.rb
@@ -5,6 +5,27 @@ require_relative "test_helper"
 Yast.import "Snapper"
 
 describe Yast::Snapper do
+  describe "GetFileModification" do
+    # Quite mocking test just for ensuring bsc#1195021 does not happen again
+    context "when there are differences" do
+      before do
+        allow(subject).to receive(:GetSnapshotPath).with(anything).and_return("")
+        allow(Yast::SCR).to receive(:Execute).with(anything, /diff/)
+          .and_return({ "stderr" => "", "stdout" => "Found differences: \xA1 \xA1" })
+        allow(Yast::SCR).to receive(:Execute).with(anything, /ls/)
+          .and_return("mode1 user1 group1 mode2 mode2 user2 group2")
+        allow(Yast::FileUtils).to receive(:Exists).and_return(true)
+      end
+
+      it "does not crash while encoding" do
+        result = subject.GetFileModification("fake_file_1", 2, 3)
+
+        expect(result).to be_a(Hash)
+        expect(result["diff"]).to include("Found differences")
+      end
+    end
+  end
+
   describe "#userdata_to_string" do
     it "call with empty userdata" do
       expect(Yast::Snapper.userdata_to_string({})).to eq("")
